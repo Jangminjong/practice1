84258357722125d5ef27af10755da295c3b9e819
mung <mung@mung.com>
2021-09-15 13:19:47 +0900

    사용자 회원가입 암호화 로직 추가

--- idwith/src/main/java/com/idwith/mpweb/user/service/UserServiceImpl.java
+++ idwith/src/main/java/com/idwith/mpweb/user/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.idwith.mpweb.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.idwith.mpweb.user.UserDAO;
@@ -14,6 +15,15 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void insertUser(UserVO vo) {
 		System.out.println("Service : 회원가입 처리");
+		
+		BCryptPasswordEncoder scpwd = new BCryptPasswordEncoder();
+        System.out.println(vo.toString());
+        //암호화 하기전
+        String password = scpwd.encode(vo.getPassword());
+        //암호화 하여 password에 저장
+        vo.setPassword(password);
+        System.out.println(vo.toString());
+        
 		dao.insertUser(vo);
 	}
 
