3dd0ee4c2d414962e39ee19118a0690a37e5f33b
sohee <dlwlsrl33@gmail.com>
2021-09-23 13:47:02 +0900

    noticeUser.js >> aboutUser.js로 파일명 변경

--- idwith/src/main/webapp/resources/admin/js/aboutUser.js
+++ idwith/src/main/webapp/resources/admin/js/aboutUser.js
@@ -1,3 +1,4 @@
+// 공지 글 작성 및 수정 페이지에 적용
 $(window).ready(function(){
 	$(document).on("click", '#notice-user-submit', function(){
 		if(document.noticeUserForm.board_title.value==""){
@@ -11,3 +12,9 @@ $(window).ready(function(){
 		}
 	});
 });
+
+// qna 페이지에서 특정 카테고리로 정렬할 때
+function setCategory(){
+	var set = $('#qna_type').val();
+	location.replace('/mpweb/qna.mdo?set='+set);
+}
